e305b63f638fafad7f96655d826ff3fb299c59c5
Tony Jiang <tonghui16@gmail.com>
2023-07-03 01:08:21 -0400

    split component and add css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ date, title, amount }) => {
@@ -6,11 +7,7 @@ const ExpenseItem = ({ date, title, amount }) => {
   const year = date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
