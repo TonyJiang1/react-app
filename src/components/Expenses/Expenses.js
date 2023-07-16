515acf1d9b49e6973a5591ab461858636535f45a
Tony Jiang <tonghui16@gmail.com>
2023-07-15 22:34:11 -0400

    assignment 2 - filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,29 +1,39 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
-  const {items} = props;
+  const { items } = props;
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
+  const changeFilterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
 
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date} ></ExpenseItem>
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date} ></ExpenseItem>
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date} ></ExpenseItem>
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date} ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler}/>
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date} ></ExpenseItem>
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date} ></ExpenseItem>
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date} ></ExpenseItem>
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date} ></ExpenseItem>
+        </Card>
+      </div>
   );
 }
 
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,13 +1,13 @@
 import './NewExpense.css';
 import { ExpenseForm } from './ExpenseForm';
 
-export const NewExpense = () => {
+export const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
-    console.log(expenseData);
+    props.onAddExpense(expenseData);
   };
 
   return <div className='new-expense'>
