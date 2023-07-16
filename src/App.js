0aa2fcf48c7187f813049f6a96faff8bff3d33b0
Tony Jiang <tonghui16@gmail.com>
2023-07-16 15:18:12 -0400

    Update filter

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Expenses from './components/Expenses/Expenses';
 import { NewExpense } from './components/NewExpense/NewExpense';
 
 const DUMMY_EXPENSES = [
-  { id: 'e1', title: 'Toilet Paper', amount: 4.67, date: new Date(2023, 5, 28) },
-  { id: 'e2', title: 'New TV', amount: 2950.67, date: new Date(2023, 6, 28) },
-  { id: 'e3', title: 'Car Insurance', amount: 296.67, date: new Date(2023, 7, 28) },
-  { id: 'e4', title: 'New Desk', amount: 397.67, date: new Date(2023, 8, 28) },
+  { id: 'e1', title: 'Toilet Paper', amount: 4.67, date: new Date(2022, 5, 28) },
+  { id: 'e2', title: 'New TV', amount: 2950.67, date: new Date(2021, 6, 28) },
+  { id: 'e3', title: 'Car Insurance', amount: 296.67, date: new Date(2021, 7, 28) },
+  { id: 'e4', title: 'New Desk', amount: 397.67, date: new Date(2020, 8, 28) },
 ];
 function App() {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,20 +10,30 @@ const Expenses = (props) => {
   
   const changeFilterHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter(expense => {
+    return expense.date.getFullYear().toString() == filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(item =>
+      <ExpenseItem
+        key={item.id}
+        title={item.title}
+        amount={item.amount}
+        date={item.date}
+      />
+    )
   }
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={changeFilterHandler} />
-        {items.map(item =>
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        )}
+        {expensesContent}
       </Card>
     </div>
   );
